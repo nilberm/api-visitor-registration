d63423d5639ccaa395e7118a5a5e8f8b10b928d0
nilberm <nilber16@outlookcom>
2024-05-11 03:23:39 -0300

    feat: added db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,23 @@ import { Module } from '@nestjs/common';
 import { VisitorModule } from './visitor/visitor.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { APP_GUARD } from '@nestjs/core';
+// import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: [`.env.state.${process.env.STAGE}`],
-    }),
+    ConfigModule.forRoot(),
     VisitorModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'visitor-registration',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
   ],
